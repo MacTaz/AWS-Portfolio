dec968a4b1d5813807d8783ef825e05758452310
MicoTazarte <micotazarte@gmail.com>
2026-08-06 20:53:34 +0800

    added dynamodb/api-gateway/lambdafunction

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
-
+import { useEffect } from 'react'
 import Sidebar from '@/components/SideBar/Sidebar.jsx'
 import ProjectSection from '@/components/ProjectSection/ProjectSection.jsx'
 import BackgroundText from '@/components/BackgroundText/BackgroundText.jsx'
 
 function App() {
+  useEffect(() => {
+    fetch("https://ioaty9p2d5.execute-api.us-east-1.amazonaws.com/log-visit", {
+      method: "POST",
+    }).catch((err) => console.error("Failed to log visit:", err));
+  }, []);
 
   return (
-    <div className="relative flex flex-col md:flex-row w-full md:h-screen font-sans">    
+    <div className="relative flex flex-col md:flex-row w-full md:h-screen font-sans">
       <BackgroundText />
       <Sidebar />
       <ProjectSection />
